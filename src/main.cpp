5439ff3cae5b156c71e8654e652f0754cc224e89
Tim Gazdic <tim.gazdic@gmail.com>
2023-11-19 13:06:56 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+int IN1 = 1;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(IN1, OUTPUT);
+    digitalWrite(IN1, LOW);
+    delay(500);
+}
+
+void loop() 
+{
+    digitalWrite(IN1, HIGH);
+    delay(10000);
+    digitalWrite(IN1, LOW);
+    delay(60000);
+}
