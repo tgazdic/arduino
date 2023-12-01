c638a279c5cd9b1574164cff4c64addea5ccb64c
Tim Gazdic <tim.gazdic@gmail.com>
2023-12-02 00:16:04 +0100

    Modifications to make experiment easier

--- single_pump.cpp
+++ single_pump.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
 
-int IN1 = 2;
+int IN1 = 5;
 
 void setup() 
 {
     Serial.begin(9600);
     pinMode(IN1, OUTPUT);
-    digitalWrite(IN1, LOW);
+    digitalWrite(IN1, HIGH);
+    delay(5000); // so it does not start spilling immediately
+
 }
 
 void loop() 
 {
-    digitalWrite(IN1, HIGH);
-    delay(1000);
     digitalWrite(IN1, LOW);
-    delay(1000);
+    delay(2000);
+    digitalWrite(IN1, HIGH);
+    delay(10000);
 }
