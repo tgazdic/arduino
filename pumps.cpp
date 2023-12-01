b9ca2c06c2e42e23407a14c9e2e9a7831068ddc2
Tim Gazdic <tim.gazdic@gmail.com>
2023-12-01 22:36:43 +0100

    Pumps script for 4 pumps. Single pump individual

--- pumps.cpp
+++ pumps.cpp
@@ -1,19 +1,49 @@
 #include <Arduino.h>
 
-int IN1 = 1;
+int IN1 = 2;
+int IN2 = 3;
+int IN3 = 4;
+int IN4 = 5;
 
 void setup() 
 {
     Serial.begin(9600);
     pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
     digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+
     delay(500);
 }
 
 void loop() 
 {
     digitalWrite(IN1, HIGH);
-    delay(10000);
+    delay(500);
     digitalWrite(IN1, LOW);
-    delay(60000);
+
+    delay(10000);
+
+    digitalWrite(IN2, HIGH);
+    delay(500);
+    digitalWrite(IN2, LOW);
+
+    delay(10000);
+
+    digitalWrite(IN3, HIGH);
+    delay(500);
+    digitalWrite(IN3, LOW);
+
+    delay(10000);
+
+    digitalWrite(IN4, HIGH);
+    delay(500);
+    digitalWrite(IN4, LOW);
+
+    delay(10000);
 }
---
+++ single_pump.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+int IN1 = 2;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(IN1, OUTPUT);
+    digitalWrite(IN1, LOW);
+    delay(500);
+}
+
+void loop() 
+{
+    digitalWrite(IN1, HIGH);
+    delay(1000);
+    digitalWrite(IN1, LOW);
+    delay(10000);
+}
