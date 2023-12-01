8d6cb030a9a27ad6c2a3c554471e3c84e0d23ebc
Tim Gazdic <tim.gazdic@gmail.com>
2023-12-01 23:05:08 +0100

    pumps adapted for reverse logic

--- pumps.cpp
+++ pumps.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 
+// Pumps are designed with reverse logic:
+// LOW -> Pump is turned on
+// HIGH -> Pump is turned off
+
 int IN1 = 2;
 int IN2 = 3;
-int IN3 = 4;
+int IN3 = 4;  // Add two more pins
 int IN4 = 5;
 
 void setup() 
@@ -10,40 +14,39 @@ void setup()
     Serial.begin(9600);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);
+    pinMode(IN3, OUTPUT);  // Initialize the new pins
     pinMode(IN4, OUTPUT);
 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-
-    delay(500);
+    // Turn off all pumps initially
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
 }
 
 void loop() 
 {
-    digitalWrite(IN1, HIGH);
-    delay(500);
+    // Pump 1
     digitalWrite(IN1, LOW);
+    delay(1000);
+    digitalWrite(IN1, HIGH);
+    delay(5000);
 
-    delay(10000);
-
-    digitalWrite(IN2, HIGH);
-    delay(500);
+    // Pump 2
     digitalWrite(IN2, LOW);
+    delay(1000);
+    digitalWrite(IN2, HIGH);
+    delay(5000);
 
-    delay(10000);
-
-    digitalWrite(IN3, HIGH);
-    delay(500);
+    // Pump 3
     digitalWrite(IN3, LOW);
+    delay(1000);
+    digitalWrite(IN3, HIGH);
+    delay(5000);
 
-    delay(10000);
-
-    digitalWrite(IN4, HIGH);
-    delay(500);
+    // Pump 4
     digitalWrite(IN4, LOW);
-
-    delay(10000);
+    delay(1000);
+    digitalWrite(IN4, HIGH);
+    delay(5000);
 }
