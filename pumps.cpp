4b512f1bb90a6c668180c029cf71e1832e87c6c8
Tim Gazdic <tim.gazdic@gmail.com>
2023-12-03 11:52:45 +0100

    Pump volume control added, variables renamed

--- pumps.cpp
+++ pumps.cpp
@@ -4,49 +4,62 @@
 // LOW -> Pump is turned on
 // HIGH -> Pump is turned off
 
-int IN1 = 2;
-int IN2 = 3;
-int IN3 = 4;  // Add two more pins
-int IN4 = 5;
+// desired pumped volumes [ml]
+int pump_1_vol = 70;
+int pump_2_vol = 70;
+int pump_3_vol = 70;
+int pump_4_vol = 70;
+
+// pump-pin connections
+int pump_1 = 2;
+int pump_2 = 3;
+int pump_3 = 4;
+int pump_4 = 5;
+
+// pump characteristics from data
+// float pump_1_mlps = 9.832744952310168;
+float pump_2_mlps = 9.832744952310168;
+float pump_3_mlps = 22.222222222222225;
+float pump_4_mlps = 18.67063492063492;
 
 void setup() 
 {
     Serial.begin(9600);
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);  // Initialize the new pins
-    pinMode(IN4, OUTPUT);
+    pinMode(pump_1, OUTPUT);
+    pinMode(pump_2, OUTPUT);
+    pinMode(pump_3, OUTPUT);  // Initialize the new pins
+    pinMode(pump_4, OUTPUT);
 
     // Turn off all pumps initially
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, HIGH);
+    digitalWrite(pump_1, HIGH);
+    digitalWrite(pump_2, HIGH);
+    digitalWrite(pump_3, HIGH);
+    digitalWrite(pump_4, HIGH);
 }
 
 void loop() 
 {
     // Pump 1
-    digitalWrite(IN1, LOW);
+    digitalWrite(pump_1, LOW);
     delay(1000);
-    digitalWrite(IN1, HIGH);
+    digitalWrite(pump_1, HIGH);
     delay(5000);
 
     // Pump 2
-    digitalWrite(IN2, LOW);
-    delay(1000);
-    digitalWrite(IN2, HIGH);
+    digitalWrite(pump_2, LOW);
+    delay((pump_2_vol/pump_2_mlps)*1000);
+    digitalWrite(pump_2, HIGH);
     delay(5000);
 
     // Pump 3
-    digitalWrite(IN3, LOW);
-    delay(1000);
-    digitalWrite(IN3, HIGH);
+    digitalWrite(pump_3, LOW);
+    delay((pump_3_vol/pump_3_mlps)*1000);
+    digitalWrite(pump_3, HIGH);
     delay(5000);
 
     // Pump 4
-    digitalWrite(IN4, LOW);
-    delay(1000);
-    digitalWrite(IN4, HIGH);
+    digitalWrite(pump_4, LOW);
+    delay((pump_4_vol/pump_4_mlps)*1000);
+    digitalWrite(pump_4, HIGH);
     delay(5000);
 }
--- single_pump.cpp
+++ single_pump.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
 
-int IN1 = 5;
+int pump_1 = 5;
 
 void setup() 
 {
     Serial.begin(9600);
-    pinMode(IN1, OUTPUT);
-    digitalWrite(IN1, HIGH);
+    pinMode(pump_1, OUTPUT);
+    digitalWrite(pump_1, HIGH);
     delay(5000); // so it does not start spilling immediately
 
 }
 
 void loop() 
 {
-    digitalWrite(IN1, LOW);
+    digitalWrite(pump_1, LOW);
     delay(2000);
-    digitalWrite(IN1, HIGH);
+    digitalWrite(pump_1, HIGH);
     delay(10000);
 }
